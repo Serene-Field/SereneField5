4bc5b3dee7f0226a9c404252825ad8e404a51620
Sadamingh <yxing11@usfca.edu>
2025-03-24 23:58:46 -0600

    fixing path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://serene-field.github.io/SereneField5/',
+	base: '/SereneField5/'
 	integrations: [mdx(), sitemap()],
 });
